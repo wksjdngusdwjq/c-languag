3e073a02cbe3d69fbc3393f011f1b7c7615118f9
Tioec ertuer <foxy070311@gmail.com>
2023-04-11 06:32:18 +0000

    fdgfd

---
+++ 47.c
@@ -0,0 +1,12 @@
+#intlude<stdio.h>
+int main() {
+
+    int num1 = 14; //변수선언 및 초기화
+    int num2 = 3;
+    int sum = 0 //초기화
+
+    sum = num1 - num2;
+    printf("%d", sum);
+    
+    return 0;
+}
